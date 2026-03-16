107cda155f0fe20fa647558c51092ec20de0ffbe
Kino <cybao292261@163.com>
2026-03-16 13:36:20 +0800

    Update telescope configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,7 +64,14 @@ require('lazy').setup({
 
   { 'godlygeek/tabular', cmd = 'Tabularize' },
 
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+  },
 
   { 'folke/todo-comments.nvim', dependencies = 'nvim-lua/plenary.nvim' },
 
